0092a2a098c1e7026506c136c3d7308f8b4e0269
Eugene Barsky <34789976+evb0110@users.noreply.github.com>
2019-02-11 21:32:19 +0300

    solved

--- __tests__/ErrorRepository.test.js
+++ __tests__/ErrorRepository.test.js
@@ -1,7 +1,13 @@
 import ErrorRepository from '../src/js/ErrorRepository';
 
-const errorRepository = new ErrorRepository;
-const errors = ['Type Error', 'Reference Error', 'Good Error', 'Bad Error', ]
-
 test('test 0, should return Type Error', () => {
+  const result = ErrorRepository.description(0);
+
+  expect(result).toBe('Type Error');
+});
+
+test('test 5, should return Unknown Error', () => {
+  const result = ErrorRepository.description(5);
+
+  expect(result).toBe('Unknown Error');
 });
